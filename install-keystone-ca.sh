abc6a1d1dd51be521677a431a47f8704660eaac3
James Page <james.page@ubuntu.com>
2014-10-07 08:17:00 +0000

    General tidy

--- install-keystone-ca.sh
+++ install-keystone-ca.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-juju scp nova-cloud-controller/0:/usr/local/share/ca-certificates/keystone_juju_ca_cert.crt .
+juju scp keystone/0:/usr/local/share/ca-certificates/keystone_juju_ca_cert.crt .
 
 sudo mkdir -p /usr/local/share/ca-certificates
 sudo cp keystone_juju_ca_cert.crt /usr/local/share/ca-certificates
