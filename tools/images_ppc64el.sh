494ec1e291f888e2420a2383d14e5a9cd5b3880d
Ryan Beisner <ryan.beisner@canonical.com>
2016-03-31 04:14:35 +0000

    Update profiles

--- tools/images_ppc64el.sh
+++ tools/images_ppc64el.sh
@@ -3,24 +3,24 @@
 
 # Download images if not already present
 mkdir -p ~/images
-[ -f ~/images/vivid-server-cloudimg-ppc64el-disk1.img ] || {
-    wget -O ~/images/vivid-server-cloudimg-ppc64el-disk1.img http://cloud-images.ubuntu.com/vivid/current/vivid-server-cloudimg-ppc64el-disk1.img
+[ -f ~/images/xenial-server-cloudimg-ppc64el-disk1.img ] || {
+    wget -O ~/images/xenial-server-cloudimg-ppc64el-disk1.img http://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-ppc64el-disk1.img
 }
-[ -f ~/images/utopic-server-cloudimg-ppc64el-disk1.img ] || {
-    wget -O ~/images/utopic-server-cloudimg-ppc64el-disk1.img http://cloud-images.ubuntu.com/utopic/current/utopic-server-cloudimg-ppc64el-disk1.img
+[ -f ~/images/wily-server-cloudimg-ppc64el-disk1.img ] || {
+    wget -O ~/images/wily-server-cloudimg-ppc64el-disk1.img http://cloud-images.ubuntu.com/wily/current/wily-server-cloudimg-ppc64el-disk1.img
 }
 [ -f ~/images/trusty-server-cloudimg-ppc64el-disk1.img ] || {
     wget -O ~/images/trusty-server-cloudimg-ppc64el-disk1.img http://cloud-images.ubuntu.com/trusty/current/trusty-server-cloudimg-ppc64el-disk1.img
 }
 
 # Upload glance images to overcloud
-glance image-create --name="vivid-ppc64el" --is-public=true --progress \
-    --container-format=bare --disk-format=qcow2 < ~/images/vivid-server-cloudimg-ppc64el-disk1.img
-glance image-create --name="utopic-ppc64el" --is-public=true --progress \
-    --container-format=bare --disk-format=qcow2 < ~/images/utopic-server-cloudimg-ppc64el-disk1.img
+glance image-create --name="xenial-ppc64el" --is-public=true --progress \
+    --container-format=bare --disk-format=qcow2 < ~/images/xenial-server-cloudimg-ppc64el-disk1.img
+glance image-create --name="wily-ppc64el" --is-public=true --progress \
+    --container-format=bare --disk-format=qcow2 < ~/images/wily-server-cloudimg-ppc64el-disk1.img
 glance image-create --name="trusty-ppc64el" --is-public=true --progress \
     --container-format=bare --disk-format=qcow2 < ~/images/trusty-server-cloudimg-ppc64el-disk1.img
 
-glance image-update --property architecture=ppc64 vivid-ppc64el
-glance image-update --property architecture=ppc64 utopic-ppc64el
+glance image-update --property architecture=ppc64 xenial-ppc64el
+glance image-update --property architecture=ppc64 wily-ppc64el
 glance image-update --property architecture=ppc64 trusty-ppc64el
