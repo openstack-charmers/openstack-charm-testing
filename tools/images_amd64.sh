24175d85249fe582e77b8036224dbda5d303d6f1
Ryan Beisner <ryan.beisner@canonical.com>
2016-04-05 01:45:18 +0000

    Update

--- tools/images_amd64.sh
+++ tools/images_amd64.sh
@@ -1,13 +1,16 @@
 #!/bin/bash -e
 # Download ppc64el images and add to glance.
 
+# Presumes cirros is available in a swift bucket
+[[ -z "$SWIFT_IP" ]] && export SWIFT_IP="10.245.161.162"
+
 # Download images if not already present
 mkdir -vp ~/images
-[ -f ~/images/vivid-server-cloudimg-amd64-disk1.img ] || {
-    wget -O ~/images/vivid-server-cloudimg-amd64-disk1.img http://cloud-images.ubuntu.com/vivid/current/vivid-server-cloudimg-amd64-disk1.img
+[ -f ~/images/xenial-server-cloudimg-amd64-disk1.img ] || {
+    wget -O ~/images/xenial-server-cloudimg-amd64-disk1.img http://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-disk1.img
 }
-[ -f ~/images/utopic-server-cloudimg-amd64-disk1.img ] || {
-    wget -O ~/images/utopic-server-cloudimg-amd64-disk1.img http://cloud-images.ubuntu.com/utopic/current/utopic-server-cloudimg-amd64-disk1.img
+[ -f ~/images/wily-server-cloudimg-amd64-disk1.img ] || {
+    wget -O ~/images/wily-server-cloudimg-amd64-disk1.img http://cloud-images.ubuntu.com/wily/current/wily-server-cloudimg-amd64-disk1.img
 }
 [ -f ~/images/trusty-server-cloudimg-amd64-disk1.img ] || {
     wget -O ~/images/trusty-server-cloudimg-amd64-disk1.img http://cloud-images.ubuntu.com/trusty/current/trusty-server-cloudimg-amd64-disk1.img
@@ -24,13 +27,20 @@ mkdir -vp ~/images
 }
 
 # Upload glance images to overcloud
-glance image-create --name="vivid" --is-public=true --progress \
-    --container-format=bare --disk-format=qcow2 < ~/images/vivid-server-cloudimg-amd64-disk1.img
-glance image-create --name="utopic" --is-public=true --progress \
-    --container-format=bare --disk-format=qcow2 < ~/images/utopic-server-cloudimg-amd64-disk1.img
-glance image-create --name="trusty" --is-public=true --progress \
+glance --os-image-api-version image-create --name="xenial" --is-public=true --progress \
+    --container-format=bare --disk-format=qcow2 < ~/images/xenial-server-cloudimg-amd64-disk1.img
+glance --os-image-api-version image-create --name="wily" --is-public=true --progress \
+    --container-format=bare --disk-format=qcow2 < ~/images/wily-server-cloudimg-amd64-disk1.img
+glance --os-image-api-version image-create --name="trusty" --is-public=true --progress \
     --container-format=bare --disk-format=qcow2 < ~/images/trusty-server-cloudimg-amd64-disk1.img
-glance image-create --name="precise" --is-public=true --progress \
+glance --os-image-api-version image-create --name="precise" --is-public=true --progress \
     --container-format=bare --disk-format=qcow2 < ~/images/precise-server-cloudimg-amd64-disk1.img
-glance image-create --name="cirros" --is-public=true  --progress \
+glance --os-image-api-version image-create --name="cirros" --is-public=true  --progress \
     --container-format=bare --disk-format=qcow2 < ~/images/cirros-0.3.4-x86_64-disk.img
+
+glance --os-image-api-version 1 image-update --property architecture=x86_64 xenial
+glance --os-image-api-version 1 image-update --property architecture=x86_64 wily
+glance --os-image-api-version 1 image-update --property architecture=x86_64 trusty
+glance --os-image-api-version 1 image-update --property architecture=x86_64 precise
+glance --os-image-api-version 1 image-update --property architecture=x86_64 cirros
+
