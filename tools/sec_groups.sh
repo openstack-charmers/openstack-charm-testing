4974b8be0dc0692d85452ce60ba40aabb8c405ec
Ryan Beisner <ryan.beisner@canonical.com>
2017-09-12 08:54:10 -0500

    Re-add admin project to secgroup tool to fix rev 304

--- tools/sec_groups.sh
+++ tools/sec_groups.sh
@@ -2,7 +2,7 @@
 # Add sec groups for basic access
 secgroup=${1:-`openstack security group list --project admin| grep default| awk '{print $2}'`}
 for port in 22 53 80 443; do
-    openstack security group rule create $secgroup --protocol tcp --remote-ip 0.0.0.0/0 --dst-port $port ||:
+    openstack security group rule create $secgroup --protocol tcp --remote-ip 0.0.0.0/0 --dst-port $port --project admin ||:
 done
 
-openstack security group rule create $secgroup --protocol icmp --remote-ip 0.0.0.0/0 ||:
+openstack security group rule create $secgroup --protocol icmp --remote-ip 0.0.0.0/0 --project admin ||:
