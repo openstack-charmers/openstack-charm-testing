e090ca073cb9da6d662c6ebb2ccb2db588481f7b
sfeole <sean.feole@canonical.com>
2018-03-23 14:08:15 -0400

    Update the tools  -- arm64 images script  -- security groups script (apply perms to all profiles)

--- tools/images_arm64.sh
+++ tools/images_arm64.sh
@@ -2,12 +2,20 @@
 # Download images and add to glance.
 
 : ${TEST_IMAGE_URL_XENIAL:="http://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-arm64-uefi1.img"}
+: ${CIRROS_IMAGE_URL_XENIAL:="http://download.cirros-cloud.net/daily/20150522/cirros-d150522-aarch64-disk.img"}
 
 mkdir -vp ~/images
 [ -f ~/images/xenial-server-cloudimg-arm64-uefi1.img ] || {
     wget -O ~/images/xenial-server-cloudimg-arm64-uefi1.img $TEST_IMAGE_URL_XENIAL
 }
 
+export http_proxy=http://squid.internal:3128
+    wget -O ~/images/cirros_test.img $CIRROS_IMAGE_URL_XENIAL
+export http_proxy=''
+
 # Create glance image
 openstack image show xenial-uefi ||\
-  openstack image create --container-format bare --disk-format qcow2 --property hw_firmware_type=uefi --file ~/images/xenial-server-cloudimg-arm64-uefi1.img xenial-uefi
+  openstack image create --public --container-format bare --disk-format qcow2 --property hw_firmware_type=uefi --file ~/images/xenial-server-cloudimg-arm64-uefi1.img xenial-uefi
+
+openstack image show xenial-cirros ||\
+  openstack image create --public --container-format bare --disk-format qcow2 --property hw_firmware_type=uefi --file ~/images/cirros_test.img xenial-cirros
