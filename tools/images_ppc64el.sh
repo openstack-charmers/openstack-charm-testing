b6d481b6c27ae91cca764ebf95b22d1ddbec6025
Andrew McLeod <andrew.mcleod@canonical.com>
2018-05-01 10:25:39 -0600

    updated arm64 and ppc64el image scripts

--- tools/images_ppc64el.sh
+++ tools/images_ppc64el.sh
@@ -2,23 +2,32 @@
 # Download ppc64el images and add to glance.
 
 # Download images if not already present
+: ${WGET_MODE}:=""
 : ${TEST_IMAGE_URL_XENIAL:="http://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-ppc64el-disk1.img"}
 : ${TEST_IMAGE2_URL_XENIAL:="http://download.cirros-cloud.net/daily/20150923/cirros-d150923-ppc64le-disk.img"}
+: ${TEST_IMAGE_NAME_XENIAL:="xenial-ppc64el"}
+: ${TEST_IMAGE2_NAME_XENIAL:="cirros-ppc64el"}
 
-mkdir -p ~/images
-[ -f ~/images/xenial-server-cloudimg-ppc64el-disk1.img ] || {
-    wget -O ~/images/xenial-server-cloudimg-ppc64el-disk1.img $TEST_IMAGE_URL_XENIAL
-}
-[ -f ~/images/cirros-d150923-ppc64le-disk.img ] || {
-    wget -O ~/images/cirros-d150923-ppc64le-disk.img $TEST_IMAGE2_URL_XENIAL
+if [ ! -d ~/images ] ; then
+        mkdir -p ~/images
+fi
+
+openstack image show ${TEST_IMAGE_NAME_XENIAL} ||
+([ -f ~/images/xenial-server-cloudimg-ppc64el-disk1.img ] || {
+    wget ${WGET_MODE} -O ~/images/xenial-server-cloudimg-ppc64el-disk1.img $TEST_IMAGE_URL_XENIAL
 }
 
-# Upload glance images to overcloud
 glance --os-image-api-version 1 image-create --name="xenial-ppc64el" --is-public=true --progress \
-    --container-format=bare --disk-format=qcow2 < ~/images/xenial-server-cloudimg-ppc64el-disk1.img
-glance --os-image-api-version 1 image-create --name="cirros-ppc64el" --is-public=true --progress \
-    --container-format=bare --disk-format=qcow2 < ~/images/cirros-d150923-ppc64le-disk.img
+        --container-format=bare --disk-format=qcow2 < ~/images/xenial-server-cloudimg-ppc64el-disk1.img
 
-# Set image architecture properties
-glance --os-image-api-version 1 image-update --property architecture=ppc64 xenial-ppc64el
-glance --os-image-api-version 1 image-update --property architecture=ppc64 cirros-ppc64el
+glance --os-image-api-version 1 image-update --property architecture=ppc64 xenial-ppc64el)
+
+openstack image show ${TEST_IMAGE2_NAME_XENIAL} ||
+([ -f ~/images/cirros-d150923-ppc64le-disk.img ] || {
+    wget ${WGET_MODE} -O ~/images/cirros-d150923-ppc64le-disk.img $TEST_IMAGE2_URL_XENIAL
+}
+
+glance --os-image-api-version 1 image-create --name="cirros-ppc64el" --is-public=true --progress \
+        --container-format=bare --disk-format=qcow2 < ~/images/cirros-d150923-ppc64le-disk.img
+
+glance --os-image-api-version 1 image-update --property architecture=ppc64 cirros-ppc64el)
